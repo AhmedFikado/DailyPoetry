b4994febf40e0ce5bf6e0984615615abf3b07150
ahmed firoum <ahmedfiroumk@gmail.com>
2025-07-23 15:37:23 +0200

    Created new file auth for login, installed argon and jsonwebtoken

--- server/src/modules/user/userRepository.ts
+++ server/src/modules/user/userRepository.ts
@@ -7,6 +7,16 @@ class UserRepository {
         FROM user`);
     return rows;
   }
+  async readByEmail(email: string) {
+    const [rows] = await databaseClient.query<Rows>(
+      `
+      SELECT * 
+      FROM user
+      WHERE email = ?`,
+      [email],
+    );
+    return rows[0];
+  }
 }
 
 export default new UserRepository();
